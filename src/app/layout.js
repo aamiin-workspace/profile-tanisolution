486824fad2b8a0d44a8d011d89b0806f35245c17
aamnnr <noeramin354@gmail.com>
2025-12-16 13:55:24 +0700

    siap deploy v1

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,29 @@
-import './globals.css';
-import { Inter } from 'next/font/google';
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Tani Solution Indonesia | Inovasi Alat Pertanian Modern',
-  description: 'Pusat solusi teknologi pertanian (Agritech), magang industri, dan kolaborasi riset di Indonesia. Produsen alat pemupukan modern.',
-  keywords: ['pertanian', 'agritech', 'magang smk', 'alat pertanian', 'tani solution', 'pupuk otomatis'],
+  title: "Tani Solution Indonesia | Inovasi Alat Pertanian Modern",
+  description:
+    "Pusat solusi teknologi pertanian (Agritech), magang industri, dan kolaborasi riset di Indonesia. Produsen alat pemupukan modern.",
+  keywords: [
+    "pertanian",
+    "agritech",
+    "magang smk",
+    "alat pertanian",
+    "tani solution",
+    "pupuk otomatis",
+  ],
   openGraph: {
-    title: 'Tani Solution Indonesia',
-    description: 'Solusi Teknologi Pertanian Masa Depan.',
+    title: "Tani Solution Indonesia",
+    description: "Solusi Teknologi Pertanian Masa Depan.",
   },
   icons: {
-    icon: '/tani.webp',
+    icon: "/tani.webp",
+  },
+  verification: {
+    google: "hkjhleoU2Q4mV7ZaBL_GuL2oVmHXiXm7qNJjMQ8d3YE",
   },
 };
 
@@ -20,7 +31,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="id">
       <head>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+        />
       </head>
       <body className={`${inter.className} bg-gray-50 text-gray-700 font-sans`}>
         {children}
