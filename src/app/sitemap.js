738befee6d4448e77d7dcf28b0e435d197bc13c7
aamnnr <noeramin354@gmail.com>
2025-12-16 14:11:09 +0700

    siap deploy v1

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,10 +1,11 @@
 export default function sitemap() {
-  const baseUrl = 'https://tanisolutionindonesia.vercel.app';
+  const baseUrl = 'https://profile-tanisolution.vercel.app';
 
   return [
     { url: baseUrl, lastModified: new Date() },
     { url: `${baseUrl}/berita`, lastModified: new Date() },
     { url: `${baseUrl}/kolaborasi`, lastModified: new Date() },
+    { url: `${baseUrl}/layanan`, lastModified: new Date() },
     { url: `${baseUrl}/tentang-kami`, lastModified: new Date() },
   ];
 }
